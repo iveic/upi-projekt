0967e4ca951b98af4746ccaab3603297b71f98dc
jkatic-pmf <58439697+jkatic-pmf@users.noreply.github.com>
2020-02-07 13:44:41 +0100

    Add files via upload

---
+++ project/entiteti/putnik.py
@@ -0,0 +1,26 @@
+class Putnik():
+
+    def __init__(self, id, ime_prezime, email):
+        self._id = id
+        self._ime_prezime = ime_prezime
+        self._email = email
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def ime_prezime(self):
+        return self._ime_prezime
+
+    @property
+    def email(self):
+        return self._email
+
+    def __str__(self):
+        return"""
+        id: {0}
+        ime_prezime: {1}
+        email: {2}
+        ------------------------
+        """.format(self._id, self._ime_prezime, self._email)
